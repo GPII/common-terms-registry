479334439d17e0ce19f16b906f8ed39e94948300
Tony <duhrer@gmail.com>
2013-12-05 17:08:57 +0100

    Simplified and updated javascript based on new HTML.

--- couchapp/trapp/_attachments/script/terms.js
+++ couchapp/trapp/_attachments/script/terms.js
@@ -99,12 +99,11 @@ $(function() {
             loggedIn : function(r) {
 		        $("#profile").couchProfile(r, {});
                 wireUpTable();
-                $("#content").show();
-                $("#no-content").hide();
             },
             loggedOut : function() {
+                $("#content").html('<div class="alert alert-danger">You must log in to view the registry.</div>');
+
                 $("#profile").html('<p>Please log in to see your profile.</p>');
-                $("#content").hide();
                 $("#no-content").show();
             }
         });
