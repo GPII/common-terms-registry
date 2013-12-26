e7324e416258c0e650f58c1c815979af32abade6
Tony <duhrer@gmail.com>
2013-12-26 17:23:34 +0100

    Fixed CTR-23 and CTR-26.

--- couchapp/trapp/updates/edit.js
+++ couchapp/trapp/updates/edit.js
@@ -2,5 +2,9 @@ function(previous, request) {
     var doc = {};
     doc = request.form;
 
+    if (doc.aliases !== undefined) {
+        doc.aliases = JSON.parse(doc.aliases);
+    }
+
     return([doc, JSON.stringify(request)]);
 }
