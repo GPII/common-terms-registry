78626b9415a56185b127315e62ed163128c7f48f
Tony <tony@rasingthefloor.org>
2014-04-25 12:26:57 +0200

    Tidied up lucene indexing to only return IDs useful in presenting original records based on the search results.

--- couchapp/lucene/fulltext/by_content/index.js
+++ couchapp/lucene/fulltext/by_content/index.js
@@ -4,17 +4,27 @@ function(doc) {
 
         var defs = "";
 
-        var keys = ["uniqueId","status","notes","uses","definition","termLabel","type","source"];
-        for (var i in keys) {
-            var field = keys[i];
+        var keysToStore = ["uniqueId","aliasOf","translationOf"];
+        for (var i in keysToStore) {
+            var field = keysToStore[i];
             if (doc[field]) {
                 ret.add(doc[field],{"field":field, "store":"yes"});
                 defs += " " + doc[field] + " ";
             }
         }
 
+
+        var keysToIndex = ["status","notes","uses","definition","termLabel","type","source"];
+        for (var i in keysToIndex) {
+            var field = keysToIndex[i];
+            if (doc[field]) {
+                ret.add(doc[field],{"field":field, "store":"no"});
+                defs += " " + doc[field] + " ";
+            }
+        }
+
         // All of the data is added to the default field so that unqualified searches match any data found in the record.
-        ret.add(defs,{"field":"default", "store": "yes"});
+        ret.add(defs,{"field":"default", "store": "no"});
 
         log.debug("indexed document '" + doc.uniqueId + "'...");
 
