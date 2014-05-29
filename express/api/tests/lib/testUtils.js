ef269132c0e9deb3ddaadc0113d12c0c9da7a4a5
Tony <tony@raisingthefloor.org>
2014-05-29 11:25:57 +0200

    Converted testUtils into a module that can take a config object so that (for example), we can use different URLs for JSON schemas in dev and production.

--- express/api/tests/lib/testUtils.js
+++ express/api/tests/lib/testUtils.js
@@ -1,67 +1,65 @@
-"use strict";
 // Utility functions for common sanity checks
-var Validator = require('jsonschema').Validator;
+"use strict";
 
-// Cache JSON Schemas for all record types for use in isSaneRecord()
-var schemaFiles = {
-    "record": "record.json",
-    "alias": "alias.json",
-    "general": "term.json",
-    "term": "term.json",
-    "transform": "transform.json",
-    "translation": "translation.json",
-    "operator": "operator.json"
-};
+module.exports = function(config) {
+    var fluid = require('infusion');
+    var testUtils = fluid.registerNamespace("gpii.ctr.api.tests.testUtils");
 
-var schemas = {};
+    testUtils.isSaneRecord = function isSaneRecord(jqUnit, record) {
+        jqUnit.assertNotNull("Record should not be null...", record);
 
-Object.keys(schemaFiles).forEach(function(key){
-    var schemaKey = schemaFiles[key];
-    try {
-        var schema = require("../../../public/schema/" + schemaKey);
-        schemas[key] = schema;
-    }
-    catch (e) {
-        console.error(JSON.stringify(e));
-    }
-});
+        if (record) {
+            jqUnit.assertNotNull("Record type should not be null...", record.type);
 
+            if (record.type) {
+                // TODO: We cannot validate records that extend the common record type at the moment because of errors in referencing
+                //    var schema = schemas[record.type.toLowerCase()];
 
-module.exports.isSaneRecord = function isSaneRecord(record) {
-    if (!record) { return false; }
-    if (!record.type) { return false; }
+                // TODO: validate against the full record type with information pulled from the config object
+                var schema = require("../../../schema/" + "record");
+                jqUnit.assertNotNull("There should be schema data for the record type (" + record.type + ")...", schema);
 
-    // TODO: We cannot validate records that extend the common record type at the moment because of errors in referencing
-//    var schema = schemas[record.type.toLowerCase()];
-
-    var schema = schemas["record"];
-    if (!schema) {
-        console.log("Could not find schema data for type '" + record.type + "'...");
-        return false;
-    }
-    try {
-        var validator = new Validator();
-        var validationOutput = validator.validate(record, schema);
-
-        if (validationOutput && validationOutput.errors && validationOutput.errors.length > 0) {
-            console.log("Record failed validation: " + JSON.stringify(validationOutput.errors));
-            return false;
+                try {
+                    var Validator = require('jsonschema').Validator;
+                    var validator = new Validator();
+                    var validationOutput = validator.validate(record, schema);
+                    if (validationOutput) {
+                        jqUnit.assertFalse("There should not be any validation errors...", validationOutput.errors && validationOutput.errors.length > 0);
+                    }
+                }
+                catch(e) {
+                    jqUnit.fail("Validation threw errors:" + e.toString());
+                }
+            }
         }
-    }
-    catch (e) {
-        console.log(e);
-        return false;
-    }
+    };
 
-    return true;
+    testUtils.isSaneResponse = function isSaneResponse(jqUnit, error, response, body) {
+        jqUnit.assertNull("No errors should be returned...",error);
+        jqUnit.assertNotNull("A response should be returned...",response);
+        jqUnit.assertNotNull("The request should include a return code...", response.statusCode);
+        jqUnit.assertNotNull("A body should be returned...", body);
+
+        // Additions in support of associating JSON Schemas with all results
+        var contentTypeHeader = response.headers["content-type"];
+        jqUnit.assertNotNull("A response should have a 'Content-Type' header...", contentTypeHeader);
+        if (contentTypeHeader) {
+            jqUnit.assertTrue("The 'Content-Type' header should contain a 'profile' link...", contentTypeHeader.indexOf("profile") !== -1);
+            jqUnit.assertTrue("The 'Content-Type' header should follow the 'type+json' pattern...", contentTypeHeader.indexOf("+json") !== -1);
+        }
+
+        var linkHeader = response.headers.link;
+        jqUnit.assertNotNull("A response should have a 'Link' header...", linkHeader);
+        if (linkHeader) {
+            jqUnit.assertTrue("The 'Link' header should contain a URL...", linkHeader.indexOf("http") !== -1);
+            jqUnit.assertTrue("The 'Link' header should indicate that the link describes the record format...", linkHeader.indexOf("describedBy") !== -1);
+        }
+
+        var jsonData = JSON.parse(body);
+        jqUnit.assertNotNull("The 'ok' variable should always be set...", jsonData.ok);
+    };
+
+    return testUtils;
 };
 
-module.exports.isSaneResponse = function isSaneResponse(jqUnit, error, response, body) {
-    jqUnit.assertNull("No errors should be returned...",error);
-    jqUnit.assertNotNull("A response should be returned...",response);
-    jqUnit.assertNotNull("The request should include a return code...", response.statusCode);
-    jqUnit.assertNotNull("A body should be returned...", body);
 
-    var jsonData = JSON.parse(body);
-    jqUnit.assertNotNull("The 'ok' variable should always be set...", jsonData.ok);
-};
