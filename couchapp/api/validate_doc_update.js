b3c63764dee04f34479ffa19d4cb25aaae9c74a0
Tony <tony@raisingthefloor.org>
2014-06-05 15:17:40 +0200

    Fixed minor error in comparison.

--- couchapp/api/validate_doc_update.js
+++ couchapp/api/validate_doc_update.js
@@ -1,7 +1,7 @@
 // Rules that control what record content can be stored in Couch for the Common Terms Registry
 function(newDoc, oldDoc, userCtx) {
     // The user has to be logged in to perform updates
-    if (!userCtx || userCtx.name == null) {
+    if (!userCtx || userCtx.name === null) {
         throw({"unauthorized" : {"summary": "You must be logged in to add or edit records."}});
     }
     else {
