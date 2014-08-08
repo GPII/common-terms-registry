142df4e427ae32fd9eec4256f65ad26272e4c7db
Tony <tony@raisingthefloor.org>
2014-08-08 15:01:04 +0200

    Added support for markdown in definition fields.

--- express/public/js/components/templates.js
+++ express/public/js/components/templates.js
@@ -1,11 +1,30 @@
 // A template handling library that brings in all Handlebars templates and partials and adds functions for more easily using them.
 //
-// Requires Handlebars.js
+// Requires Handlebars.js and Pagedown (for markdown rendering)
 
 (function ($, fluid) {
     "use strict";
     var templates = fluid.registerNamespace("ctr.components.templates");
     templates.compiled = {};
+
+
+    // Add support for markdown if pagedown is available
+    templates.mdHelper = function(options) {
+        if (Markdown && Markdown.getSanitizingConverter) {
+            var converter = Markdown.getSanitizingConverter();
+            // Double all single carriage returns so that they result in new paragraphs, at least for now
+            converter.hooks.chain("preConversion", function (text) { return text.replace(/[\r\n]+/g, "\n\n"); });
+            return converter.makeHtml(options.fn(this));
+        }
+        else {
+            console.log("Pagedown or one of its dependencies is not available, so markdown will be passed on without any changes.");
+        }
+
+        // If we can't evolve the output, we just pass it through.
+        return options.fn(context);
+    };
+
+    Handlebars.registerHelper('md', templates.mdHelper);
 
     templates.render = function(key,context) {
         // If a template exists, load that.  Otherwise, try to load the partial.
