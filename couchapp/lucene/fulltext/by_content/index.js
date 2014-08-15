8b05d873bc68fde443a821918030444accdf0500
Tony <tony@raisingthefloor.org>
2014-08-15 15:06:46 +0200

    Improved search integration configuration to match more things more sensibly:  http://the-t-in-rtf.github.io/couch-lucene.html

--- couchapp/lucene/fulltext/by_content/index.js
+++ couchapp/lucene/fulltext/by_content/index.js
@@ -4,7 +4,9 @@ function(doc) {
 
         var defs = "";
 
-        var keysToStore = ["uniqueId","aliasOf","translationOf","type"];
+        // We have to store values we wish to use in field:value queries
+        // We also include them in the value of the defaults field (used when no qualifiers are added)
+        var keysToStore = ["uniqueId","aliasOf","translationOf"];
         for (var i in keysToStore) {
             var field = keysToStore[i];
             if (doc[field]) {
@@ -13,17 +15,26 @@ function(doc) {
             }
         }
 
-        var keysToIndex = ["status","notes","uses","definition","termLabel","source"];
+        // The type and status should not be included in the defs, but should be stored
+        var keysToStoreLowerCase = ["type", "status"];
+        for (var i in keysToStoreLowerCase) {
+            var field = keysToStoreLowerCase[i];
+            if (doc[field]) {
+                ret.add(doc[field].toLowerCase(),{"field":field, "store":"yes"});
+            }
+        }
+
+        var keysToIndex = ["notes","uses","definition","termLabel","source"];
         for (var i in keysToIndex) {
             var field = keysToIndex[i];
             if (doc[field]) {
                 ret.add(doc[field],{"field":field, "store":"no"});
-                defs += " " + doc[field] + " ";
+                defs += " " + doc[field].toLowerCase() + " ";
             }
         }
 
         // All of the data is added to the default field so that unqualified searches match any data found in the record.
-        ret.add(defs,{"field":"default", "store": "no"});
+        ret.add(defs,{"field":"default", "store": "yes"});
 
         log.debug("indexed document '" + doc.uniqueId + "'...");
 
