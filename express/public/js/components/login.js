6d0b04a0ac0bc5d141bbd3d01aaaf53a84dad775
Tony <tony@raisingthefloor.org>
2014-08-22 10:45:25 +0200

    Created initial "login" module to handle login form.

---
+++ express/public/js/components/login.js
@@ -0,0 +1,87 @@
+// The main search module that allows users to view the Preference Terms Dictionary
+
+(function ($) {
+    "use strict";
+    var login       = fluid.registerNamespace("ctr.components.login");
+    var templates   = fluid.registerNamespace("ctr.components.templates");
+
+    // Try to log in and display the results
+    login.submit = function(that, event) {
+        // Clear out any previous feedback before submitting
+        $(that.container).find(".alert-box").remove();
+
+        if (event) { event.preventDefault(); }
+        var name = that.locate("name").val();
+        var password = that.locate("password").val();
+        var settings = {
+            type:    "POST",
+            url:     that.options.apiUrl + "/signin",
+            success: that.displayReceipt,
+            error:   that.displayError,
+            data: { name: name, "password": password }
+        };
+
+        $.ajax(settings);
+    };
+
+    login.displayError = function(that, jqXHR, textStatus, errorThrown) {
+        templates.prependTo(that.locate("form"),"error",{message: errorThrown});
+    };
+
+    login.displayReceipt = function(that, responseData, textStatus, jqXHR) {
+        var jsonData = JSON.parse(responseData);
+        if (jsonData && jsonData.ok) {
+            templates.replaceWith(that.locate("viewport"),"success", { message: "You have succesfully logged in."});
+
+            // update our common session data regarding the logged in user, so that other components can pick up changes.
+            that.model.user = responseData.user;
+        }
+        else {
+            templates.prependTo(that.locate("form"),"error",{message: jsonData.message});
+        }
+    };
+
+    fluid.defaults("ctr.components.login", {
+        gradeNames: ["fluid.viewRelayComponent", "autoInit"],
+        model: {
+            "user": "{ctr.components.data}.model.user"
+        },
+        apiUrl: "/api/user",
+        selectors: {
+            "form":     ".login-form",
+            "viewport": ".ptd-viewport",
+            "name":     "input[name='username']",
+            "password": "input[name='password']"
+        },
+        events: {
+            "submit":   "preventable"
+        },
+        invokers: {
+            "submit": {
+                funcName: "ctr.components.login.submit",
+                args: [ "{that}", "{arguments}.0"]
+            },
+            "displayError": {
+                funcName: "ctr.components.login.displayError",
+                args: ["{that}", "{arguments}.0", "{arguments}.1", "{arguments}.2"]
+            },
+            "displayReceipt": {
+                funcName: "ctr.components.login.displayReceipt",
+                args: ["{that}", "{arguments}.0", "{arguments}.1", "{arguments}.2"]
+            }
+        },
+        listeners: {
+            onCreate: [
+                {
+                    "this": "{that}.dom.form",
+                    method: "submit",
+                    args:   "{that}.submit"
+                }
+            ],
+            "submit": {
+                func: "ctr.components.login.submit",
+                args: [ "{that}"]
+            }
+        }
+    });
+})(jQuery);
