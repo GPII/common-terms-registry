76a4cd6b4a463fdf54015d83a3b2fcd8b0bdf5a9
Tony <tony@raisingthefloor.org>
2014-09-18 12:05:21 +0200

    Updated test utils to use new common validation as part of CTR-98.

--- express/api/tests/lib/testUtils.js
+++ express/api/tests/lib/testUtils.js
@@ -2,8 +2,9 @@
 "use strict";
 
 module.exports = function(config) {
-    var fluid = require('infusion');
-    var testUtils = fluid.registerNamespace("gpii.ctr.api.tests.testUtils");
+    var fluid        = require('infusion');
+    var testUtils    = fluid.registerNamespace("gpii.ctr.api.tests.testUtils");
+    testUtils.helper = require("../../../schema/lib/schema-helper")(config);
 
     testUtils.isSaneRecord = function isSaneRecord(jqUnit, record) {
         jqUnit.assertNotNull("Record should not be null...", record);
@@ -12,30 +13,13 @@ module.exports = function(config) {
             jqUnit.assertNotNull("Record type should not be null...", record.type);
 
             if (record.type) {
-                // TODO:  The current validator apparently allows additional parameters (such as _id and _rev) not specified in the schema.
-
-                // TODO:  We cannot screen these out sanely until PouchDB (our test db) properly supports the require function
+                // TODO:  We cannot screen these out until PouchDB (our test db) properly supports the require function.
+                // See: https://github.com/pouchdb/pouchdb/issues/973
 //                jqUnit.assertNull("There should not be an '_id' parameter returned as part of our records...",record._id);
 //                jqUnit.assertNull("There should not be a '_rev' parameter returned as part of our records...",record._rev);
 
-                // TODO: We cannot validate records that extend the common record type at the moment because of errors in referencing
-                //    var schema = schemas[record.type.toLowerCase()];
-
-                // TODO: validate against the full record type with information pulled from the config object
-                var schema = require("../../../schema/schemas/" + "record");
-                jqUnit.assertNotNull("There should be schema data for the record type (" + record.type + ")...", schema);
-
-                try {
-                    var Validator = require('jsonschema').Validator;
-                    var validator = new Validator();
-                    var validationOutput = validator.validate(record, schema);
-                    if (validationOutput) {
-                        jqUnit.assertFalse("There should not be any validation errors...", validationOutput.errors && validationOutput.errors.length > 0);
-                    }
-                }
-                catch(e) {
-                    jqUnit.fail("Validation threw errors:" + e.toString());
-                }
+                var errors = testUtils.helper.validate(record.type, record);
+                jqUnit.assertUndefined("There should not be any validation errors:" + JSON.stringify(errors), errors);
             }
         }
     };
