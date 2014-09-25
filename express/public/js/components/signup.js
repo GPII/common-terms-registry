8851d63881118b4438878e71a2fc4f4311488c18
Tony <tony@raisingthefloor.org>
2014-09-25 13:40:52 +0200

    Fixed minor goof with error messages to prevent extra markup from creeping into the document when the form is submitted multiple times.

--- express/public/js/components/signup.js
+++ express/public/js/components/signup.js
@@ -45,7 +45,7 @@
             console.log("jQuery.ajax call returned meaningless jqXHR.responseText payload. Using 'errorThrown' instead.");
         }
 
-        templates.prependTo(that.locate("form"),"error",{message: message});
+        templates.prependTo(that.locate("form"),"common-error",message);
     };
 
     signup.displayReceipt = function(that, responseData, textStatus, jqXHR) {
@@ -57,7 +57,7 @@
             that.controls.refresh(that);
         }
         else {
-            templates.prependTo(that.locate("form"),"error",{message: jsonData.message});
+            templates.prependTo(that.locate("form"),"common-error", jsonData.message);
         }
     };
 
