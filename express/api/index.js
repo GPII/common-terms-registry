9509c983a32819a2db63ee267ba8850a35930a05
Tony Atkins <duhrer@gmail.com>
2015-04-22 12:26:11 +0200

    Tidied up based on JSHint feedback.

--- express/api/index.js
+++ express/api/index.js
@@ -1,71 +1,75 @@
-module.exports = function(config) {
-    var express = require('express');
+// An express router module that wraps all API functions together.  Currently sub-modules are transitioning to using
+// `gpii.express.router`, eventually this will also be based on that.
+//
+"use strict";
+module.exports = function (config) {
+    var express = require("express");
     var router = express.Router();
 
-    var search = require('./search')(config);
-    router.use('/search', search);
+    var search = require("./search")(config);
+    router.use("/search", search);
 
     var suggestConfig = JSON.parse(JSON.stringify(config));
     suggestConfig.lookup = true;
 
-    var suggest = require('./search')(suggestConfig);
-    router.use('/suggest', suggest);
+    var suggest = require("./search")(suggestConfig);
+    router.use("/suggest", suggest);
 
     // Record lookup end points, one for all types, and one each per type
-    var records = require('./records')(config);
-    router.use('/records', records);
+    var records = require("./records")(config);
+    router.use("/records", records);
 
     var termsConfig = JSON.parse(JSON.stringify(config));
     termsConfig.recordType = "term";
     var terms = require("./records")(termsConfig);
-    router.use('/terms', terms);
+    router.use("/terms", terms);
 
     var aliasConfig = JSON.parse(JSON.stringify(config));
     aliasConfig.recordType = "alias";
     var aliases = require("./records")(aliasConfig);
-    router.use('/aliases', aliases);
+    router.use("/aliases", aliases);
 
     var transformConfig = JSON.parse(JSON.stringify(config));
     transformConfig.recordType = "transform";
     var transforms = require("./records")(transformConfig);
-    router.use('/transforms', transforms);
+    router.use("/transforms", transforms);
 
     var translationConfig = JSON.parse(JSON.stringify(config));
     translationConfig.recordType = "translation";
     var translations = require("./records")(translationConfig);
-    router.use('/translations', translations);
+    router.use("/translations", translations);
 
     var conditionsConfig = JSON.parse(JSON.stringify(config));
     conditionsConfig.recordType = "condition";
     var conditions = require("./records")(conditionsConfig);
-    router.use('/conditions', conditions);
+    router.use("/conditions", conditions);
 
-    var record = require('./record')(config);
-    router.use('/record', record);
+    var record = require("./record")(config);
+    router.use("/record", record);
 
     // Display the API docs for everything else
-    router.use("/",function(req, res) {
+    router.use("/", function (req, res) {
         if (req.path === "/") {
-            var marked = require('marked');
+            var marked = require("marked");
 
             var markdown = "";
-            var fs = require('fs');
+            var fs = require("fs");
             var BUF_LENGTH = 64*1024;
             var buffer = new Buffer(BUF_LENGTH);
-            var mdFile = fs.openSync(__dirname + "/ctr.md", 'r');
+            var mdFile = fs.openSync(__dirname + "/ctr.md", "r");
             var bytesRead = 1;
             var pos = 0;
             while (bytesRead > 0) {
                 bytesRead = fs.readSync(mdFile, buffer, 0, BUF_LENGTH, pos);
-                markdown += buffer.toString('utf8', 0, bytesRead);
+                markdown += buffer.toString("utf8", 0, bytesRead);
                 pos += bytesRead;
             }
             fs.closeSync(mdFile);
 
-            res.render('pages/page', { "title": "API Documentation", "body": marked(markdown)});
+            res.render("pages/page", { "title": "API Documentation", "body": marked(markdown)});
         }
         else {
-            res.status(404).render('pages/error', {message: "The page you requested was not found."});
+            res.status(404).render("pages/error", {message: "The page you requested was not found."});
         }
     });
     return router;
