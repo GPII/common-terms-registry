9a6d49b2d6878e70c20dc105bded873c3d08c20b
Tony Atkins <duhrer@gmail.com>
2015-04-23 14:49:19 +0200

    Tidied up code based on feedback from JSHint (no functional changes).

--- express/api/record/get/index.js
+++ express/api/record/get/index.js
@@ -1,7 +1,7 @@
 "use strict";
 
-module.exports = function(config) {
-    var fluid = require('infusion');
+module.exports = function (config) {
+    var fluid = require("infusion");
 
     var schemaHelper = require("../../../schema/lib/schema-helper")(config);
     var namespace    = "gpii.ctr.record.get";
@@ -9,15 +9,15 @@ module.exports = function(config) {
     var record       = fluid.registerNamespace(namespace);
     record.schema    = "record";
     record.error     = require("../../lib/error")(config);
-    var children     = require('../../lib/children')(config, record);
+    var children     = require("../../lib/children")(config);
 
-    var express      = require('express');
+    var express      = require("express");
     var filters      = require("secure-filters");
 
 
     var router = express.Router();
 
-    router.get('/:uniqueId', function(req, res){
+    router.get("/:uniqueId", function (req, res) {
         // per-request variables need to be defined here, otherwise (for example) the results of the previous search will be returned if the next search has no records
         record.req = req;
         record.res = res;
@@ -34,7 +34,7 @@ module.exports = function(config) {
         };
 
         // Server config validation
-        if (!config || !config['couch.url']) {
+        if (!config || !config["couch.url"]) {
             var message = "Your instance is not configured correctly to enable record lookup.  You must have a couch.url variable configured.";
             console.log(message);
             return res.status(500).send(JSON.stringify({ ok:false, message: message }));
@@ -43,7 +43,7 @@ module.exports = function(config) {
         // User input validation
         try {
             if (!record.req.params || !record.req.params.uniqueId) {
-                throw record.error.constructError(400,"You must provide the required query parameters to use this interface.");
+                throw record.error.constructError(400, "You must provide the required query parameters to use this interface.");
             }
 
             if (record.req.query.children) {
@@ -56,13 +56,13 @@ module.exports = function(config) {
 
         // Get the record from couch
         var sanitizedId = filters.uri(record.req.params.uniqueId);
-        var request = require('request');
-        request.get(config['couch.url'] + "/_design/api/_view/entries?keys=%5B%22" + sanitizedId + "%22%5D", function(e,r,b) {
-            if (e) { return res.status(500).send(JSON.stringify({ok: false, message: "Error retrieving record from couchdb", error: e}));}
+        var request = require("request");
+        request.get(config["couch.url"] + "/_design/api/_view/entries?keys=%5B%22" + sanitizedId + "%22%5D", function (e, r, b) {
+            if (e) { return res.status(500).send(JSON.stringify({ok: false, message: "Error retrieving record from couchdb", error: e})); }
 
             var jsonData = JSON.parse(b);
             if (!jsonData.rows || jsonData.rows.length === 0) {
-                return res.status(404).send(JSON.stringify({ok:false, message: "A record with the unique ID '" + sanitizedId + "' could not be found." }));
+                return res.status(404).send(JSON.stringify({ok: false, message: "A record with the unique ID '" + sanitizedId + "' could not be found." }));
             }
             else {
                 record.results.record = jsonData.rows[0].value;
@@ -75,22 +75,22 @@ module.exports = function(config) {
 
                     // retrieve the child records via /tr/_design/api/_view/children?key=
                     var childRecordOptions = {
-                        "url" : config['couch.url'] + "/_design/api/_view/children?key=%22" + sanitizedParentId + "%22",
+                        "url" : config["couch.url"] + "/_design/api/_view/children?key=%22" + sanitizedParentId + "%22",
                         "json": true
                     };
 
-                    var request = require('request');
+                    var request = require("request");
                     request.get(childRecordOptions, record.getChildRecords);
                 }
                 else {
                     schemaHelper.setHeaders(res, record.results.record.type);
-                    res.status(200).send( JSON.stringify(record.results));
+                    res.status(200).send(JSON.stringify(record.results));
                 }
             }
         });
     });
 
-    router.get("/", function(req,res) {
+    router.get("/", function (req, res) {
         schemaHelper.setHeaders(res, "message");
         return res.status(400).send({ok: false, message: "You must provide the required uniqueId in the path to use this interface."});
     });
--- express/schema/lib/schema-helper.js
+++ express/schema/lib/schema-helper.js
@@ -15,7 +15,7 @@
 //   TODO:  This will eventually need to be extracted to a small standard component with more extensive tests, and reused in the Unified Listing.
 
 "use strict";
-var fluid = require('infusion');
+var fluid = require("infusion");
 var gpii  = fluid.registerNamespace("gpii");
 
 fluid.registerNamespace("gpii.schema.helper");
@@ -26,8 +26,8 @@ var url     = require("url");
 var ZSchema = require("z-schema");
 
 // Cache all our schemas on startup.
-gpii.schema.helper.init = function(that) {
-    fluid.each(that.options.schemas, function(schemaPath, schemaName){
+gpii.schema.helper.init = function (that) {
+    fluid.each(that.options.schemas, function (schemaPath, schemaName) {
         var schemaContent = require(schemaPath);
         that.cache[schemaName] = schemaContent;
     });
@@ -39,13 +39,13 @@ gpii.schema.helper.init = function(that) {
 // to return the appropriate headers.
 gpii.schema.helper.getSchemaHeaders = function (that, key) {
     if (!that.options.baseUrl) {
-        fluid.fail("You must provide a base URL option before you can use this component to set headers");
+        fluid.fail("You must provide a base URL option before you can use this component to construct headers.");
     }
 
     var schemaUrl = url.resolve(that.options.baseUrl, key + ".json");
 
     return {
-        "Content-Type": 'application/' + key + '+json; profile=' + schemaUrl,
+        "Content-Type": "application/" + key + "+json; profile=" + schemaUrl,
         "Link": schemaUrl + "#; rel=\"describedBy\""
     };
 };
@@ -60,12 +60,12 @@ gpii.schema.helper.setSchemaHeaders = function (that, res, key) {
 
     fluid.log("WARNING:  Your code is using the old method of setting schema headers, and should be updated.");
     var schemaHeaders = that.getSchemaHeaders(key);
-    fluid.each(schemaHeaders, function(value, key){
+    fluid.each(schemaHeaders, function (value, key) {
         res.set(key, value);
     });
 };
 
-gpii.schema.helper.validate = function(that, key, content) {
+gpii.schema.helper.validate = function (that, key, content) {
     if (!that.options.schemas) {
         fluid.fail("You must provide a map of schema names and paths to use this module.");
     }
@@ -74,7 +74,7 @@ gpii.schema.helper.validate = function(that, key, content) {
     var validator = new ZSchema(that.options.zSchemaOptions);
 
     // We have to validate the schemas themselves before we can validate records.
-    var schemasValid = validator.validateSchema(Object.keys(that.cache).map(function(v) { return that.cache[v]; }));
+    var schemasValid = validator.validateSchema(Object.keys(that.cache).map(function (v) { return that.cache[v]; }));
     if (!schemasValid) {
         return (gpii.schema.helper.sanitizeValidationErrors(that, validator.getLastErrors()));
     }
@@ -115,18 +115,18 @@ gpii.schema.helper.validate = function(that, key, content) {
      pattern-based matches like uniqueId, which z-schema represents in an obtuse format.  This function also breaks
      down validation errors by field so that we can show feedback in-context.
  */
-gpii.schema.helper.sanitizeValidationErrors = function(that, errors) {
+gpii.schema.helper.sanitizeValidationErrors = function (that, errors) {
     var saneErrors = {};
 
-    errors.forEach(function(error){
+    errors.forEach(function (error) {
         // Errors with fields that contain data are already associated with the field based on the path
-        var field = error.path.replace("#/","");
+        var field = error.path.replace("#/", "");
 
         // Document-level failures about missing fields need to associated with the field based on the params
         if (error.code === "OBJECT_MISSING_REQUIRED_PROPERTY") { field = error.params[0]; }
 
         // We could have multiple validation errors for a single field, so we need to allow arrays
-        if (!saneErrors[field]) { saneErrors[field]=[];}
+        if (!saneErrors[field]) { saneErrors[field] = []; }
         saneErrors[field].push(error.message);
     });
 
@@ -136,13 +136,12 @@ gpii.schema.helper.sanitizeValidationErrors = function(that, errors) {
 // A convenience function to convert a list of schema names into the new options format.
 // This will likely be removed once all local code has been converted to Fluid components.
 // TODO: Remove this once we are a general module and not working with our own local schemas
-gpii.schema.helper.constructSchemas = function(schemaNames) {
+gpii.schema.helper.constructSchemas = function (schemaNames) {
     var schemaBasePath = path.resolve(__dirname, "../schemas/");
 
     var schemaConfig = {};
-    schemaNames.map(function(schemaName){
+    schemaNames.map(function (schemaName) {
         var schemaFilename = schemaName + ".json";
-        var record = {};
         schemaConfig[schemaName] =  path.resolve(schemaBasePath, schemaFilename);
     });
 
@@ -152,7 +151,7 @@ gpii.schema.helper.constructSchemas = function(schemaNames) {
 // We look up and then create a map of all templates for our defaults.  To override this, pass in `options.schemas` as a map of keys and full paths.
 var schemaBasePath  = path.resolve(__dirname, "../schemas/");
 var schemaFileNames = fs.readdirSync(schemaBasePath);
-var schemaNames     = schemaFileNames.map(function(filename){ return filename.substring(0, filename.indexOf(".json"));});
+var schemaNames     = schemaFileNames.map(function (filename) { return filename.substring(0, filename.indexOf(".json")); });
 
 fluid.defaults("gpii.schema.helper", {
     gradeNames: ["fluid.eventedComponent", "autoInit"],
@@ -195,7 +194,7 @@ fluid.defaults("gpii.schema.helper", {
 });
 
 // Transitional "shim" to allow using this module with the previous configuration syntax until all local code is converted to Fluid components.
-module.exports = function(config) {
+module.exports = function (config) {
     return gpii.schema.helper({
         baseUrl: url.resolve(config["base.url"], "schema")
     });
