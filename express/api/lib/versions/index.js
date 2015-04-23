0c95414864713f05431329cbb9f27873ff3c3a42
Tony Atkins <duhrer@gmail.com>
2015-04-23 14:54:33 +0200

    Tidied up placeholder comment (no functional changes).

--- express/api/lib/versions/index.js
+++ express/api/lib/versions/index.js
@@ -1,6 +1,8 @@
 // Library to add consistent handling of creating and displaying versions
 "use strict";
 
-module.exports = function(config) {
+// TODO:  This is a placeholder, and will eventually be replaced by a working implementation.
+
+module.exports = function (config) {
 
 };
